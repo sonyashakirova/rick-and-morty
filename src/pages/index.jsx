81db99ec3231a25e6029b89e31217fad490e08f5
Sonya Shakirova <smshakirova@mail.ru>
2023-07-16 18:11:12 +0300

    feat: add routing and home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./home"
+
 export function Pages() {
   return (
-    <div></div>
+    <div className="app-wrapper">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
