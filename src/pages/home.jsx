b9c6b45839433afd77210c3894eb6e8b62e5e1e6
Sonya Shakirova <smshakirova@mail.ru>
2023-07-16 18:25:10 +0300

    feat: add home page picture

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,11 @@ export function Home() {
   return (
     <div>
       <h1 className="visually-hidden">Home</h1>
+      <img
+        className="home-image"
+        src="../images/rick-and-morty.png"
+        alt="Rick and Morty"
+      />
     </div>
   )
 }
