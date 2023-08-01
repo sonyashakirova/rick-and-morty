bb313266b6d13574e4cc7b4cdfa2f922ee23809b
Sonya Shakirova <smshakirova@mail.ru>
2023-08-01 21:41:22 +0300

    feat: add auth provider

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import { Navigation } from "./components/navigation";
+import { AuthProvider } from "./context/auth-provider";
 import { Pages } from "./pages";
 import "./styles.css"
 
 export function App() {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Navigation />
-        <Pages />
-      </div>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="app-wrapper">
+          <Navigation />
+          <Pages />
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
