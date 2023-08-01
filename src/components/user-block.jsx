005733f79a4c481337050c055511a0f21bfd5db9
Sonya Shakirova <smshakirova@mail.ru>
2023-08-01 21:47:17 +0300

    feat: add user block

---
+++ src/components/user-block.jsx
@@ -0,0 +1,27 @@
+import { Link, NavLink, useNavigate } from "react-router-dom"
+import { useAuth } from "../context/auth-provider"
+
+export function UserBlock() {
+  const { user, logout } = useAuth()
+  const navigate = useNavigate()
+
+  function handleClick() {
+    logout(() => navigate("/"))
+  }
+
+  return (
+    <header className="user-block">
+      {user === null
+        ? <NavLink className="button-ghost" to="/login">
+            Login
+          </NavLink>
+        : <div>
+            <p>Hello, {user}!</p>
+            <button className="button-ghost" type="button" onClick={handleClick}>
+              Logout
+            </button>
+          </div>
+      }
+    </header>
+  )
+}
