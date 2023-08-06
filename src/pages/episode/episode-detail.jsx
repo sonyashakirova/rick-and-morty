d80cffde4920c67f3374abea86829769b819e287
Sonya Shakirova <smshakirova@mail.ru>
2023-08-06 15:33:52 +0300

    refactor: add category card components

--- src/pages/episode/episode-detail.jsx
+++ src/pages/episode/episode-detail.jsx
@@ -1,15 +1,23 @@
 import { useParams } from "react-router-dom"
 import episodes from "../../data/episode.json"
 
+function EpisodeCard({ episode }) {
+  return (
+    <div>
+      <h1 className="title">{episode.name}</h1>
+      <p>Episode: {episode.episode}</p>
+      <p>Air date: {episode.air_date}</p>
+    </div>
+  )
+}
+
 function EpisodeDetail() {
   const { id } = useParams()
   const episode = episodes.find((item) => item.id.toString() === id)
   
   return (
     <div className="content-wrapper">
-      <h1 className="title">{episode.name}</h1>
-      <p>Episode: {episode.episode}</p>
-      <p>Air date: {episode.air_date}</p>
+      <EpisodeCard episode={episode} />
     </div>
   )
 }
--- src/pages/location/location-detail.jsx
+++ src/pages/location/location-detail.jsx
@@ -1,15 +1,23 @@
 import { useParams } from "react-router-dom"
 import locations from "../../data/location.json"
 
+function LocationCard({ location }) {
+  return (
+    <div>
+      <h1 className="title">{location.name}</h1>
+      <p>Type: {location.type}</p>
+      <p>Dimension: {location.dimension}</p>
+    </div>
+  )
+}
+
 function LocationDetail() {
   const { id } = useParams()
   const location = locations.find((item) => item.id.toString() === id)
   
   return (
     <div className="content-wrapper">
-      <h1 className="title">{location.name}</h1>
-      <p>Type: {location.type}</p>
-      <p>Dimension: {location.dimension}</p>
+      <LocationCard location={location} />
     </div>
   )
 }
