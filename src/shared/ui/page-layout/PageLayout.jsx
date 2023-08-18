34d2c163e53c67734c94b3a7c382d92ff5313fda
Sonya Shakirova <smshakirova@mail.ru>
2023-08-18 17:27:10 +0300

    feat: add adaptability

--- src/shared/ui/page-layout/PageLayout.jsx
+++ src/shared/ui/page-layout/PageLayout.jsx
@@ -1,10 +1,43 @@
+import { useState } from "react"
 import { Outlet } from "react-router"
-import { Logo, Navigation, UserBlock } from "shared/ui"
+import { ActionIcon, Burger } from "@mantine/core"
+import { ReactComponent as UserIcon } from "shared/images/user.svg"
+import { Drawer, Logo, Navigation, UserBlock } from "shared/ui"
 import "./PageLayout.css"
 
 export function PageLayout({ children }) {
+  const [openNavigation, setOpenNavigation] = useState(false)
+  const [openUserBlock, setOpenUserBlock] = useState(false)
+
   return (
     <div className="layout">
+      <div className="layout-header">
+        <div>
+          <Burger
+            opened={openNavigation}
+            onClick={() => {
+              if (openUserBlock) setOpenUserBlock(false)
+              setOpenNavigation(o => !o)
+            }}
+            size="md"
+          />
+          <Drawer from="left" opened={openNavigation} onClick={() => setOpenNavigation(false)}>
+            <Navigation closeMenu={() => openNavigation(false)} />
+          </Drawer>
+        </div>
+        <Logo />
+        <div>
+          <ActionIcon onClick={() => {
+            if (openNavigation) setOpenNavigation(false)
+            setOpenUserBlock(o => !o)
+          }}>
+            <UserIcon fill="#15778a" />
+          </ActionIcon>
+          <Drawer from="right" opened={openUserBlock} onClick={() => setOpenUserBlock(false)}>
+            <UserBlock />
+          </Drawer>
+        </div>
+      </div>
       <div className="layout-sidebar">
         <Logo />
         <Navigation />
